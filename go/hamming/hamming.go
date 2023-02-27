1170584aeac3168ce6bfb840b698e3717a7da1d1
Jonathan Reichert <jonathan.reichert@gmail.com>
2023-02-26 20:03:05 -0600

    hamming complete

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,17 @@
 package hamming
 
-func Distance(a, b string) (int, error) {
+import "errors"
 
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("lengths not equal")
+	}
+	dist := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			dist++
+		}
+	}
+
+	return dist, nil
 }
